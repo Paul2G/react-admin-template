unempco/track-system-client

feat(project): add router constants to project config

src/project.config.ts
@@ -20,4 +20,7 @@ export default {
     url: import.meta.env?.VITE_BASE_API_URL,
     path: '/api',
   },
+  router: {
+    defaultRoute: '/app/transactions',
+  },
 } as const;

Paul2G/react-admin-template

(cherry picked from commit 43bb024c0fcc759cb51df8bc4748ea51d7dfc8bc)

src/project.config.ts
@@ -20,4 +20,7 @@ export default {
     url: import.meta.env?.VITE_BASE_API_URL,
     path: '/api',
   },
+  router: {
+    defaultRoute: '/app/dashboard',
+  },
 } as const;